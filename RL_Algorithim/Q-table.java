4ed28f5f611c1239a191a553bef6e18914ca28fe
Rosnel14 <leyvarosnel@gmail.com>
2020-10-07 13:11:59 -0400

    Create Q-table.java
    Q-learning algorithim set up

---
+++ RL_Algorithim/Q-table.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.robotcontroller
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Q-Table", group="RL_Testing")
+//@Disabled
+public class Q-table extends LinearOpMdoe() {
+
+    //environment, possibly using a gyroscope (power module has built in gyroscope)
+
+    DcMotor leftFront;
+    DcMotor leftBack;
+    DcMotor rightFront;
+    DcMotor rightBack;
+    Gyro
+
+
+    public void runOpMode() {
+
+        }
+
+        }
