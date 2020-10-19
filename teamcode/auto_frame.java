d41083978f4f21eff75518df439cc927ae9fa0b4
Rosnel14 <leyvarosnel@gmail.com>
2020-10-19 16:59:24 -0400

    Create auto_frame.java

---
+++ teamcode/auto_frame.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Pushbot: Auto Drive By Time", group="Pushbot")
+@Disabled
+
