be16a4e508ec264acfc6f68ff014c1b04d99fb3e
Rosnel14 <leyvarosnel@gmail.com>
2020-10-26 16:30:28 -0400

    deleted redundancies

--- teamcode/Drive_Control.java
+++ teamcode/Drive_Control.java
@@ -1,53 +1,88 @@
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 
 
 @TeleOp(name="Demo Drive Controls", group="Iterative Opmode")
 //@Disabled
-
-public class Drive_Control extends OpMode {
+public class Drive_Control extends LinearOpMode {
 
     // Declare vars (components)
     //motors are defined if taken from a top down view
-    //Xavier was here
 
     DcMotor leftBack;
     DcMotor leftFront;
     DcMotor rightBack;
     DcMotor rightFront;
+    int dpishift = 2;
 
 
     @Override
     public void runOpMode(){
         leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
-        runtime.reset();
+        //runtime.reset();
 
         waitForStart();
 
         while(opModeIsActive()) {
-            double gamma = 1.5; //meant to counteract imperfect strafing, driver preference
-            double y = -gamepad1.left_stick_y; //reversed
-            double x = gamepad1.right_stick._x * gamma;
-            double rx = gamepad1.right_stick_x;
 
-            leftFront.setPower(y + x + rx);
-            leftBack.setPower(y - x + rx);
-            rightFront.setPower(y - x - rx); 
-            rightBack.setPower(y + x - rx);
+            if (gamepad1.right_stick_x != 0) {
+
+                if (gamepad1.left_bumper) {
+
+                    dpishift = 10;
+                }
+                else {
+                    dpishift = 2;
+                }
+            }
+
+            if (gamepad1.left_bumper) {
+
+                dpishift = 5;
+            } else {
+
+                dpishift = 1;
+            }
+
+
+            leftBack.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x + (gamepad1.right_stick_x)) / dpishift);
+
+            rightBack.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x - (gamepad1.right_stick_x)) / dpishift);
+
+            leftFront.setPower((gamepad1.left_stick_y + gamepad1.left_stick_x - (gamepad1.right_stick_x)) / dpishift);
+
+            rightFront.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x + (gamepad1.right_stick_x)) / dpishift);
 
         }
 
