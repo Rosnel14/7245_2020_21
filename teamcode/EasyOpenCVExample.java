f8ee44d72641b70d26895312cdb51dade099479f
Rosnel Alejandro Leyva-Cortes <30304057+Rosnel14@users.noreply.github.com>
2021-03-03 16:40:33 -0500

    re size

--- teamcode/EasyOpenCVExample.java
+++ teamcode/EasyOpenCVExample.java
@@ -79,7 +79,7 @@ public class EasyOpenCVExample extends LinearOpMode
         /*
          * The core values which define the location and size of the sample regions
          */
-        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(120,205);
+        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(120,190);
 
         static final int REGION_WIDTH = 35;
         static final int REGION_HEIGHT = 25;
